b23cc956555c36d6235ac2b007dbe82a0a41e3c1
Andres Gorgona <74382181+MrManito@users.noreply.github.com>
2023-04-16 14:10:02 -0600

    Primer archivo del proyecto

---
+++ Computadora.py
@@ -0,0 +1,21 @@
+class Computer:
+    # class attribute
+    cores=1
+    instruc_sec=1
+    disk_access_tieme=5 #segundos
+    ram=400 #kb
+    #hard_disk=indefinido
+    
+    #instance attribute
+    def __init__(self, name):
+        self.name = name
+        
+    # instance method
+    def specs(self):
+        print(f"Las especificaciones de la computadora {self.name} son: \n ->Nucleos de procesamiento: {self.cores} \n ->Instrucciones por segundo: {self.instruc_sec} \n ->Tiempo de acceso al disco: {self.disk_access_tieme}s \n ->RAM: {self.ram}kb \n ->Memoria en disco ilimitada")
+
+emu_computer = Computer("HP")
+
+emu_computer.specs()
+
+
